132aeddd195b4634f4833383996f00477803f7c1
Mohd Rizwan <morizwanansar786@gmail.com>
2019-02-27 18:43:45 -0800

    Some changes

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 $('.hamburger a').click(function(){
     $('.side').toggleClass('side-toggle');
 })
-
 $('.close a').click(function(){
     $('.side').toggleClass('side-toggle');
+})
+
+$(window).resize(function(e){
+    e.preventDefault();
 })
